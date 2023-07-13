3fc907e52fc79cfb7abeb7f8ebd4e70139020d16
unknown <nhj25252@gmail.com>
2023-07-13 14:16:50 +0900

    [css] format CV

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -250,6 +250,9 @@ function App() {
   return (
     <>
       <form onSubmit={handleSubmit}>
+        <button type="submit" className="saveCV editing" onClick={toggleSaveCV}>
+          Save CV
+        </button>
         <Personal onChange={handleChange} />
         <Section sectionName="TECHNICAL SKILLS" />
         <textarea
@@ -286,9 +289,6 @@ function App() {
           value={state.interests}
           placeholder="Technical Writing; UX Design; Photography; Baking"
         />
-        <button type="submit" className="saveCV editing" onClick={toggleSaveCV}>
-          Save CV
-        </button>
       </form>
     </>
   );
